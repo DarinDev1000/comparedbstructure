2ea711489a9f512c5917438eddee15a4ced14452
Darin <darindev1000@gmail.com>
2018-11-01 14:57:17 -0700

    Changed Databases object structure. Created Tables sub-object.

--- models/dbcompare.js
+++ models/dbcompare.js
@@ -23,21 +23,22 @@ class DBCompare {
     // console.log(db2Tables);
     // console.log(typeof db1Tables);
     // console.log(typeof db2Tables);
-    Databases.databaseName1 = databaseName1;
-    Databases.databaseName2 = databaseName2;
-    Databases.db1Tables = db1Tables;
-    Databases.db2Tables = db2Tables;
+    Databases.db1.databaseName = databaseName1;
+    Databases.db2.databaseName = databaseName2;
+    Databases.db1.tablesObject = db1Tables;
+    Databases.db2.tablesObject = db2Tables;
   }
 
-  static async getAllTableList(Databases, Tables) {
+  static async getAllTableList(Databases, allTableSet) {
     console.log("Running getAllTableList");
     // Generate tableList1
-    for (let index = 0; index < Databases.db1Tables.length; index++) {
+    for (let index = 0; index < Databases.db1.tablesObject.length; index++) {
       try {
         // console.log(db1Tables[index]);
         // console.log(db1Tables[index].TABLE_NAME);
-        Tables.tableList1.push(Databases.db1Tables[index].TABLE_NAME);
-        Tables.allTableSet.add(Databases.db1Tables[index].TABLE_NAME);
+        const tableName = Databases.db1.tablesObject[index].TABLE_NAME
+        Databases.db1.tableList.push(tableName);
+        allTableSet.add(tableName);
       } catch (error) {
         console.log("TABLE_NAME error.  Is object undefined?");
       }
@@ -47,25 +48,27 @@ class DBCompare {
     }
 
     // Generate tableList2
-    for (let index = 0; index < Databases.db2Tables.length; index++) {
+    for (let index = 0; index < Databases.db2.tablesObject.length; index++) {
       try {
-        // console.log(db2Tables[index]);
-        // console.log(db2Tables[index].TABLE_NAME);
-        Tables.tableList2.push(Databases.db2Tables[index].TABLE_NAME);
-        Tables.allTableSet.add(Databases.db2Tables[index].TABLE_NAME);
+        const tableName = Databases.db2.tablesObject[index].TABLE_NAME
+        Databases.db2.tableList.push(tableName);
+        allTableSet.add(tableName);
+        console.log(tableName);
+        console.log(allTableSet);
       } catch (error) {
         console.log("TABLE_NAME error.  Is object undefined?");
       }
     }
     // SORTING IS NOT NECESSARY. THE DATABASE IF ALPHABETICAL BY DEFAULT.
-    Tables.tableList1.sort;
-    Tables.tableList2.sort;
-    Tables.allTableSet.sort;
+    Databases.db1.tableList.sort;
+    Databases.db2.tableList.sort;
+    // allTableSet.sort;
     // console.log(Tables.tableList1);
     // console.log(Tables.tableList2);
-    // console.log(Tables.allTableSet);
+    console.log(allTableSet);
 
-
+    // COPY Databases.db1.allTableSet TO DB2
+    //Databases.db2.allTableSet = Databases.db1.allTableSet;
   }
 
   static async getTableColumns(Databases) {
@@ -103,19 +106,23 @@ class DBCompare {
       // ONE WAY TO MAKE OBJECTS
       // MAKE Databases OBJECT
       var Databases = new Object();
-      // QUESTION: should I remove these?
-      Databases.databaseName1 = "databaseName1";
-      Databases.databaseName2 = "databaseName2";
-      Databases.db1Tables = ["db1Tables"];
-      Databases.db2Tables = ["db2Tables"];
-      Databases.db1 = { "tableList": []};
-      Databases.db2 = { "tableList": []};
-      Databases.allTableSet = new Set([]);
+      Databases.db1 = {};
+      Databases.db2 = {};
+      Databases.db1.databaseName = "databaseName1";
+      Databases.db2.databaseName = "databaseName2";
+      Databases.db1.tablesObject = ["db1Tables"];
+      Databases.db2.tablesObject = ["db2Tables"];
+      Databases.db1.tableList = [];
+      Databases.db2.tableList = [];
+      // Databases.db1.allTableSet = new Set([]);
+      // Databases.db2.allTableSet = new Set([]);
       Databases.db1.Tables = {};
       Databases.db2.Tables = {};
-      Databases.db1.Tables.columnList = [];
-      Databases.db2.Tables.columnList = [];
 
+      var allTableSet = new Set();
+
+
+      /* // moved to Databases object
       // ANOTHER WAY TO MAKE OBJECTS
       // MAKE Tables OBJECT
       var Tables = {
@@ -123,21 +130,32 @@ class DBCompare {
         tableList2: [],
         allTableSet: new Set([])
       };
+      */
 
       // GET TABLE LIST FROM THE DATABASE
       await DBCompare.getDatabaseTables(Databases);
-      await DBCompare.getAllTableList(Databases, Tables);
+      await DBCompare.getAllTableList(Databases, allTableSet);
       //console.log(Databases);
-      console.log(Tables);
+
+      // SET TABLE OBJECTS
+      for (const t of Databases.db1.tableList) {
+        Databases.db1.Tables[t] = {};
+        Databases.db1.Tables[t].columnList = [];
+        console.log({t});
+      }
+      for (const t of Databases.db2.tableList) Databases.db2.Tables[t] = {};
+      console.log(Databases);
 
       // TRUE/FALSE : ARE THE TABLES THE SAME
-      const tablesSame = await ObjectCompare.isEqual(Tables.tableList1, Tables.tableList2) ? true : false;
+      const tablesSame = await ObjectCompare.isEqual(Databases.db1.tableList, Databases.db2.tableList1) ? true : false;
       console.log({tablesSame});
 
       // OUTPUT THE TABLES TO HTML
       if (tablesSame) await ctx.render('dbcompare.twig', {tableSame: true, color: 'green', result1: Databases.db1Tables, result2: Databases.db2Tables, tablesSameResult: "Yes, The databases have the same tables."});
       else await ctx.render('dbcompare.twig', {tableSame: false, color: 'red', result1: Databases.db1Tables, result2: Databases.db2Tables, tablesSameResult: "NO, The databases have different tables."});
 
+      // VIEW JSON OUTPUT ***** comment out *****
+      ctx.body = Databases;
 
       /*
       // ------ USING isEqual library -------
@@ -161,56 +179,64 @@ class DBCompare {
       //-----------------------------------------------------
       console.log("\n","Compare columns in tables");
 
-      // Converts object to string to compare equality.
-      for (let index = 0; index < Databases.db1Tables.length; index++) {
-        try {
-          //console.log(db1Tables[index]);
-          console.log(Databases.db1Tables[index].TABLE_NAME);
-          //console.log(db2Tables[index]);
-          console.log(Databases.db2Tables[index].TABLE_NAME);
-        } catch (error) {
-          console.log("TABLE_NAME error.  Is object undefined?");
-        }
+
+
+
+
+
+
+
+
+      // // Converts object to string to compare equality.
+      // for (let index = 0; index < Databases.db1.tablesObject.length; index++) {
+      //   try {
+      //     //console.log(db1Tables[index]);
+      //     console.log(Databases.db1Tables[index].TABLE_NAME);
+      //     //console.log(db2Tables[index]);
+      //     console.log(Databases.db2Tables[index].TABLE_NAME);
+      //   } catch (error) {
+      //     console.log("TABLE_NAME error.  Is object undefined?");
+      //   }
         
-        if ( (Databases.db1Tables[index] && Databases.db1Tables[index].TABLE_NAME) === (Databases.db2Tables[index] && Databases.db2Tables[index].TABLE_NAME) ) {
-          console.log("same");
+      //   if ( (Databases.db1Tables[index] && Databases.db1Tables[index].TABLE_NAME) === (Databases.db2Tables[index] && Databases.db2Tables[index].TABLE_NAME) ) {
+      //     console.log("same");
 
-          //-----------------------------------------------------
-          // GET COLUMNS OF EACH TABLE
-          //-----------------------------------------------------
+      //     //-----------------------------------------------------
+      //     // GET COLUMNS OF EACH TABLE
+      //     //-----------------------------------------------------
 
-          // GET COLUMNS OF COMPARE DB 1
-          if (Databases.db1Tables[index]) {
-            //const databaseName1 = process.env.DB_COMPARE_1_DATABASE;  // already done
-            const tableName1 = Databases.db1Tables[index].TABLE_NAME;
-            var [db1TableColumns] = await global.comparedb1.query(`SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = :tableName1 AND TABLE_SCHEMA=:databaseName1;`,
-                                                      {tableName1: tableName1, databaseName1: Databases.databaseName1});
-            //console.log(db1TableColumns);
-          }
+      //     // GET COLUMNS OF COMPARE DB 1
+      //     if (Databases.db1Tables[index]) {
+      //       //const databaseName1 = process.env.DB_COMPARE_1_DATABASE;  // already done
+      //       const tableName1 = Databases.db1Tables[index].TABLE_NAME;
+      //       var [db1TableColumns] = await global.comparedb1.query(`SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = :tableName1 AND TABLE_SCHEMA=:databaseName1;`,
+      //                                                 {tableName1: tableName1, databaseName1: Databases.db1.databaseName});
+      //       //console.log(db1TableColumns);
+      //     }
 
-          // GET COLUMNS OF COMPARE DB 2
-          if (Databases.db2Tables[index]) {
-            //const databaseName2 = process.env.DB_COMPARE_2_DATABASE;  // already done
-            const tableName2 = Databases.db2Tables[index].TABLE_NAME;
-            var [db2TableColumns] = await global.comparedb2.query(`SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = :tableName2 AND TABLE_SCHEMA=:databaseName2;`,
-                                                      {tableName2: tableName2, databaseName2: Databases.databaseName2});
-            //console.log(db2TableColumns);
-          }
+      //     // GET COLUMNS OF COMPARE DB 2
+      //     if (Databases.db2Tables[index]) {
+      //       //const databaseName2 = process.env.DB_COMPARE_2_DATABASE;  // already done
+      //       const tableName2 = Databases.db2Tables[index].TABLE_NAME;
+      //       var [db2TableColumns] = await global.comparedb2.query(`SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = :tableName2 AND TABLE_SCHEMA=:databaseName2;`,
+      //                                                 {tableName2: tableName2, databaseName2: Databases.db2.databaseName});
+      //       //console.log(db2TableColumns);
+      //     }
 
-          if ( _.isEqual(db1TableColumns, db2TableColumns) ){
-            console.log("Columns are same");
-          } else {
-            console.log("Columns are NOT the same");
-          }
+      //     if ( _.isEqual(db1TableColumns, db2TableColumns) ){
+      //       console.log("Columns are same");
+      //     } else {
+      //       console.log("Columns are NOT the same");
+      //     }
           
-          // for (let col = 0; col < db1TableColumns.length; col++) {
-          //   console.log(db1TableColumns[col]);
-          // }
+      //     // for (let col = 0; col < db1TableColumns.length; col++) {
+      //     //   console.log(db1TableColumns[col]);
+      //     // }
 
-        } else 
-        console.log("different");
+      //   } else 
+      //   console.log("different");
       
-      }
+      // }
 
 
 
